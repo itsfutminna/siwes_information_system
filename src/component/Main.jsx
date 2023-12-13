15b3a6d7e91909ad9e71929980a8ecbb4d11f88c
ADAGA HAPPY <happyadaga@gmail.com>
2023-12-13 11:29:56 +0100

    added style to form validation error

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -6,7 +6,8 @@ export default function Main() {
     <main className="bg-white flex-col">
       {/* section 1 */}
       <div className=" py-20 h-screen  bg-slate-500">
-        <div className=" items-center justify-center w-10/12 mx-auto gap-4 md:flex ">
+        {/* items-center justify-center w-10/12 mx-auto gap-4 md:flex */}
+        <div className="justify-center w-10/12 mx-auto gap-4 md:flex">
           {/* heading */}
           <div className="text-slate-100 font-bold basis-1/2 text-center md:text-start ">
             <p className="text-4xl">
